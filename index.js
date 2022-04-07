3a88fa1a0e8a3b9baf46569ba4c47e91f68a0326
Woo Yeongyun <woo036325@gmail.com>
2022-04-08 01:05:59 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { Client } = require('discord.js')
+const express = require('express')
+const expressHandlebars = require("express-handlebars")
+
+const { token } = require(__dirname + '/dev/config.json')
+const app = express()
+const PORT = 3000
+
+app.engine('handlebars', expressHandlebars.engine());
+app.set('view engine', 'handlebars')
+
+const client = new Client({ intents: [] })
+client.token = token
+
+app.get('/', (req, res) => {
+	if (req.query.id) {
+		client.users.fetch(req.query.id).then((user) => {
+			res.json(JSON.stringify(user));
+		})
+	} else {
+		res.send('No id provided');
+	}
+})
+
+app.get('/profile', (req, res) => {
+	//res.sendFile(__dirname + '/src/profile.html')
+	if (req.query.id) {
+		client.users.fetch(req.query.id).then((user) => {
+			res.render('profile',
+				{
+					layout: null,
+					username: user.username,
+					discriminator: user.discriminator,
+					avatar: user.displayAvatarURL(),
+				})
+		})
+	} else {
+		res.send('No id provided');
+	}
+});
+
+app.get('/ginto.woff', (req, res) => {
+	res.sendFile(__dirname + '/src/ginto.woff')
+})
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
+})
